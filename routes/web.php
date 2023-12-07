8f5ad242ae6cae598c409d71d16091e98cf06d94
Den Kong <denusklo@gmail.com>
2023-12-07 17:56:13 +0000

    update composer and demo mail

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PHPMailer\PHPMailer\PHPMailer;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/phpmailer', function () {
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->SMTPDebug = 0;
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = 'tls';
+    $mail->Username = config('mail.mailers.smtp.username');
+    $mail->Password = config('mail.mailers.smtp.password');
+    $mail->setFrom('info@denusklo.com', 'Your Name');
+    $mail->addReplyTo('info@denusklo.com', 'Your Name');
+    $mail->addAddress('denusklo@gmail.com', 'Receiver Name');
+    $mail->Subject = 'Checking if PHPMailer works';
+    // $mail->msgHTML(file_get_contents('message.html'), __DIR__);
+    $mail->Body = 'This is just a plain text message body';
+    $mail->addAttachment('attachment.txt');
+    if (!$mail->send()) {
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'The email message was sent.';
+    }
 });
